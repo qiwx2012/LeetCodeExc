269e2e91ee40a0051046d7b6ce1f2ee2dcfef5d9
qiwx <476364523@qq.com>
2018-12-27 22:28:24 +0800

    验证合法括号

---
+++ src/com/qiwx/stack/IsValid.java
@@ -0,0 +1,39 @@
+package com.qiwx.stack;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+//有效括号
+public class IsValid {
+
+    public static void main(String[] args) {
+        System.out.println(isValid("()"));
+       // System.out.println(isValid("()[]{}"));
+        //System.out.println(isValid("(]"));
+        //System.out.println(isValid("{[]}"));
+    }
+    private static boolean isValid(String s){
+        if(s==null||s.length()==1)
+            return false;
+        Map<Character,Character> map=new HashMap<>();
+        map.put(')','(');
+        map.put(']','[');
+        map.put('}','{');
+        char[] str=s.toCharArray();
+        Stack<Character> stacks=new Stack<>();
+        for(int i=0;i<str.length;i++) {
+            char c = s.charAt(i);
+            if (map.containsValue(c)) {
+                stacks.push(c);
+            } else {
+                char topElement = stacks.isEmpty() ? '1' : stacks.pop();
+                if (topElement != map.get(c)) {
+                    return false;
+                }
+            }
+        }
+        return stacks.isEmpty();
+
+    }
+}
