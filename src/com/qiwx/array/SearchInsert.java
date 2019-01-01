4cc4ae503d33a92c2a71996e5eeb8a59b8e0d703
qiwx <476364523@qq.com>
2019-01-01 19:04:49 +0800

    搜索插入位置 二分查找完成后对插入位置的判断，根据mid索引对应数组的值跟marget值比较大小，如果是大于marget则插入位置mid+1，如果是小于marget插入值就是mid。

---
+++ src/com/qiwx/array/SearchInsert.java
@@ -0,0 +1,33 @@
+package com.qiwx.array;
+//搜索插入位置
+public class SearchInsert {
+    public static void main(String[] args) {
+       // System.out.println(searchInsert(new int[]{1,3,5,6},5));
+        System.out.println(searchInsert(new int[]{1,3,5,6},2));
+        System.out.println(searchInsert(new int[]{1,3,5,6},7));
+        System.out.println(searchInsert(new int[]{1,3,5,6},0));
+        System.out.println(searchInsert(new int[]{1,3},2));
+    }
+    public static int searchInsert(int[] nums, int target) {
+        int len=nums.length;
+        int left=0;
+        int right=len-1;
+        int mid=(left+right)/2;
+        while (left<right){
+            if(nums[mid]>target){
+                right=mid-1;
+            }else if(nums[mid]<target){
+                left=mid+1;
+            }else{
+               return mid;
+            }
+            mid=(left+right)/2;
+        }
+        if(nums[mid]<target){
+            return mid+1;
+        }else{
+            return mid;
+        }
+    }
+
+}
