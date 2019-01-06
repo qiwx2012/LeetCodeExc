c7960f279a1653a4469b217a6a05c16089d14e4e
qiwx <476364523@qq.com>
2019-01-06 22:00:47 +0800

    最后一个单词长度，两种方法 一个注意的地方是最后一个空格，还有注意利用字符串的分割函数。

---
+++ src/com/qiwx/string/LengthOfLastWord.java
@@ -0,0 +1,33 @@
+package com.qiwx.string;
+//最后一个单词的长度
+public class LengthOfLastWord {
+    public static void main(String[] args) {
+        System.out.println(lengthOfLastWord1("Hello World"));
+        System.out.println(lengthOfLastWord1("a "));
+        System.out.println(lengthOfLastWord1("  "));
+    }
+    private static int lengthOfLastWord1(String s){
+        String[] str=s.split(" ");
+        if(str.length==0)
+            return 0;
+        else {
+            return str[str.length-1].length();
+        }
+    }
+    private static int lengthOfLastWord(String s){
+        if(s==null||s.length()==0)
+            return 0;
+        int count=0;
+        s=s.trim();
+        int len=s.length();
+        for(int i=len-1;i>=0;i--){
+            if(s.charAt(i)!=' '){
+                count++;
+            }else {
+                return count;
+            }
+
+        }
+        return count;
+    }
+}
