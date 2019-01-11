1efb2f37b5af3cd8e2cec32f376133a590f8b07d
祁维晓 <qiwx@jingzhengu.com>
2019-01-11 17:57:34 +0800

    余9法是最简单的方法，一行代码搞定，涉及到数学原理。

---
+++ src/com/qiwx/num/AddDigits.java
@@ -0,0 +1,21 @@
+package com.qiwx.num;
+//各位相加
+public class AddDigits {
+    public static void main(String[] args) {
+        addDigits(38);
+    }
+    //有个一种一行代码搞定的 (num-1)%9+1;数学原理清楚余9法
+    public static int addDigits(int num) {
+        if(num/10==0)
+            return num;
+        int temp=0;
+        while (num!=0){
+            temp+=num%10;
+            num=num/10;
+        }
+        num=addDigits(temp);
+        return num;
+
+    }
+
+}
