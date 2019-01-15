d5a4ac380a3b9d6f2eaf68f319eeedf4615d6753
qiwx <476364523@qq.com>
2019-01-15 23:44:03 +0800

    股票最大收益

---
+++ src/com/qiwx/array/MaxProfit2.java
@@ -0,0 +1,17 @@
+package com.qiwx.array;
+//买卖股票的最佳时机 II
+public class MaxProfit2 {
+    public static void main(String[] args) {
+    }
+    public static int maxProfit(int[] prices) {
+        int len=prices.length;
+        int max=0;
+        for(int i=0;i<len-1;i++){
+            if(prices[i]<prices[i+1]){
+                max=max+prices[i+1]-prices[i];
+            }
+
+        }
+        return max;
+    }
+}
