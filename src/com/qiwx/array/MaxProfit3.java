8a094f748f62b685ba2238b7787b6b0a60741dea
qiwx <qiwx@jingzhengu.com>
2019-01-16 15:03:55 +0800

    股票最大利益3

--- src/com/qiwx/array/MaxProfit3.java
+++ src/com/qiwx/array/MaxProfit3.java
@@ -7,9 +7,10 @@ public class MaxProfit3 {
     public static void main(String[] args) {
         //maxProfit(new int[]{3,3,5,0,0,3,1,4});
        // maxProfit(new int[]{1,4,2});
-        maxProfit(new int[]{2,1,4});
+       // maxProfit(new int[]{2,1,4});
        // maxProfit(new int[]{1,2,3,4,5});
         //maxProfit(new int[]{7,6,4,2,1});
+        maxProfit(new int[]{1,2,4,2,5,7,2,4,9,0});
        // maxProfit(new int[]{1,3,5,4,3,7,6,9,2,4});
         //maxProfit(new int[]{1,2,3,4,5});
     }
@@ -18,36 +19,26 @@ public class MaxProfit3 {
         int len=prices.length;
         if(len==0)
             return 0;
-        int bottom=prices[0];
-        int[] prices1=new int[len+1];
-        System.arraycopy(prices,0,prices1,0,len);
-        prices1[len]=-1;
-        int top=-1;
+        int buy=prices[0];
+        int sell=-1;
         Stack<Integer> stack=new Stack<>();
-        for(int i=0;i<len;i++){
-              if(prices1[i]<=prices1[i+1]){
-                  //下行趋势只记录谷底
-                  bottom=prices1[i];
-              }else{
-                 stack.push(prices1[i]-bottom);
-              }
-        }
-        if(top>=0&&bottom>=0){
-            stack.push(top-bottom);
-
-        }else if(top<0){
-            if(stack.size()<=0){
-                //说明死线性递增
-                stack.push(prices[len-1]-prices[0]);
+        for(int i=1;i<len;i++){
+            if(prices[i]>prices[i-1]){
+                sell=prices[i];
+                if(i==len-1){
+                    stack.push(sell-buy);
+                }
             }else{
-                if(prices[len-1]>bottom){
-                    stack.push(prices[len-1]-bottom);
+                if(buy>=0&&sell>=0){
+                   int a=sell-buy;
+                   stack.push(a);
+                   buy=prices[i];
+                   sell=-1;
+                }else{
+                    buy=prices[i];
                 }
 
             }
-
-        }else if(bottom<0){
-            stack.clear();
         }
         if(stack.size()==0){
             return 0;
@@ -56,27 +47,23 @@ public class MaxProfit3 {
         }else if(stack.size()==2){
             return stack.elementAt(0)+stack.elementAt(1);
         }else {
-               int index=-1;
-               int mm=-1;
-               int size=0;
-               while (size<2){
-                   for(int i=0;i<stack.size();i++){
-                        if(mm<=stack.elementAt(i)){
-                            mm=stack.elementAt(i);
-                            index=i;
-                        }
-                   }
-                   max+=mm;
-                   mm=-1;
-                   stack.remove(index);
-                   size++;
+            int index=-1;
+            int mm=-1;
+            int size=0;
+            while (size<2){
+                for(int i=0;i<stack.size();i++){
+                    if(mm<=stack.elementAt(i)){
+                        mm=stack.elementAt(i);
+                        index=i;
+                    }
+                }
+                max+=mm;
+                mm=-1;
+                stack.remove(index);
+                size++;
 
-               }
-               return max;
-
-
+            }
+            return max;
         }
-
-
     }
 }
