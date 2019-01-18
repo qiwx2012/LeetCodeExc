2de5d35229ddd1d683e2afd435f4e2d5fe2eda9e
qiwx <476364523@qq.com>
2019-01-18 22:35:22 +0800

    存在重复元素，对于只存储一个value可以使用Hashset代替HashMap，更节约时间，HashSet也比Stack节省时间，算法要尽量减少运算时间

---
+++ src/com/qiwx/array/ContainsDuplicate.java
@@ -0,0 +1,37 @@
+package com.qiwx.array;
+
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+     System.out.println(containsDuplicate(new int[]{3,3}));
+    }
+    private static boolean containsDuplicate(int[] nums){
+        if (nums==null)
+            return false;
+        int len=nums.length;
+        if(len<=1)
+            return false;
+        HashSet<Integer> maps=new HashSet<>();
+        for(int i=1;i<len;i++){
+            if(!maps.contains(nums[i])){
+                maps.add(nums[i]);
+            }else {
+                return false;
+            }
+        }
+        return false;
+    }
+    //此方法跟节省时间
+    private static boolean containDulicate2(int[] nums){
+        int len=nums.length;
+        if(len==0)
+            return false;
+        int num=nums[0];
+        HashSet<Integer> maps=new HashSet<>();
+        for(int i=0;i<len;i++){
+            maps.add(nums[i]);
+        }
+        return  maps.size()!=len;
+    }
+}
