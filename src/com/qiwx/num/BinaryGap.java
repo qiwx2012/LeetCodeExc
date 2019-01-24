bb55b1d649fb4417b07798ec6698a3d0ba688f8d
qiwx <qiwx@jingzhengu.com>
2019-01-24 18:11:38 +0800

    条件判断的顺序 能够影响程序的执行速度

---
+++ src/com/qiwx/num/BinaryGap.java
@@ -0,0 +1,66 @@
+package com.qiwx.num;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//二进制间距
+public class BinaryGap {
+    public static void main(String[] args) {
+        System.out.println(binaryGap1(22));
+        System.out.println(binaryGap1(8));
+        System.out.println(binaryGap1(6));
+        System.out.println(binaryGap1(5));
+    }
+    public static int binaryGap1(int N) {
+        int maxGap=0;
+        int start=0;
+        while (N>0){
+            if(N%2==0){
+                if(start!=0){
+                    start++;
+                }
+                N=N>>1;
+            }else{
+                if(start==0){
+                    start++;
+                    N=N>>1;
+                }else{
+                    //如果第二次发现1，直接比较返回值，将大值付给maxGap，
+                    //然后此种情况N值不变，继续进行下一轮
+                    if(maxGap<start){
+                        maxGap=start;
+                    }
+                    start=0;
+                }
+            }
+        }
+        return maxGap;
+
+    }
+    public static int binaryGap(int N) {
+        int maxGap=0;
+        Map<Integer,Integer> maps=new HashMap<>();
+        while (N>0){
+            if(N%2!=0){
+                if(maps.isEmpty()){
+                    maps.put(1,1);
+                    N=N>>1;
+                }else{
+                    //如果第二次发现1，直接比较返回值，将大值付给maxGap，
+                    //然后此种情况N值不变，继续进行下一轮
+                    if(maxGap<maps.get(1).intValue()){
+                        maxGap=maps.get(1).intValue();
+                    }
+                    maps.clear();
+                }
+            }else{
+                if(!maps.isEmpty()){
+                    maps.put(1,maps.get(1).intValue()+1);
+                }
+                N=N>>1;
+            }
+        }
+        return maxGap;
+
+    }
+}
