2440cc137c26f7f3d6a0e9c81fa97d4ace89ec25
qiwx <476364523@qq.com>
2019-02-01 22:37:09 +0800

    最长字符串

---
+++ src/com/qiwx/string/LongestWord.java
@@ -0,0 +1,27 @@
+package com.qiwx.string;
+//词典中最长单词
+public class LongestWord {
+    public static void main(String[] args) {
+        System.out.println(longestWord(new String[]{"a", "banana", "app", "appl", "ap", "apply", "apple"}));
+    }
+    private static String longestWord(String[] words){
+        String result="";
+        int len=result.length();
+        for(int i=0;i<30;i++){
+
+            if(words[i].length()>len){
+                result=words[i];
+                len=result.length();
+            }else if(words[i].length()==len){
+                for(int j=0;j<len;j++){
+                    if(words[i].charAt(j)<result.charAt(j)){
+                        result=words[i];
+                        break;
+                    }
+                }
+
+            }
+        }
+        return result;
+    }
+}
