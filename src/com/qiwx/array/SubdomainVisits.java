1bf1b064ecb4a9484af66be90a1a1db5bd105ed9
qiwx <476364523@qq.com>
2019-02-06 20:45:13 +0800

    学生出勤记录

---
+++ src/com/qiwx/array/SubdomainVisits.java
@@ -0,0 +1,37 @@
+package com.qiwx.array;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+//子域名访问计数
+public class SubdomainVisits {
+    public static void main(String[] args) {
+           System.out.println(subdomainVisits(new String[]{"9001 discuss.leetcode.com"}));
+    }
+    public static List<String> subdomainVisits(String[] cpdomains) {
+        Map<String,Integer> maps=new HashMap<>();
+        int len=cpdomains.length;
+        for(int i=0;i<len;i++){
+            String[] temp=cpdomains[i].split(" ");
+            int times=Integer.valueOf(temp[0]);
+            //域名全程
+            maps.put(temp[1],maps.getOrDefault(temp[1],0)+times);
+            //以此取出上级域名
+            int start=0;
+            while (temp[1].indexOf(".",start)>0){
+                int index=temp[1].indexOf(".",start);
+                String tt=temp[1].substring(index+1);
+                maps.put(tt,maps.getOrDefault(tt,0)+times);
+                start=index+1;
+            }
+        }
+        List<String> result=new LinkedList<>();
+        for (String s:maps.keySet()) {
+             result.add(maps.get(s)+" "+s);
+        }
+        return result;
+
+    }
+}
---
+++ src/com/qiwx/string/CheckRecord.java
@@ -0,0 +1,30 @@
+package com.qiwx.string;
+//学生出勤记录1
+public class CheckRecord {
+    public static void main(String[] args) {
+         System.out.println(checkRecord("LALL"));
+         System.out.println(checkRecord("PPALLL"));
+    }
+        private static boolean checkRecord(String s){
+        int absent=0;//缺勤次数大于1不合格
+        int late=0;//迟到次数大于2不合格
+        char[] chars=s.toCharArray();
+        for(int i=0;i<chars.length;i++){
+            if(chars[i]=='A'){
+                absent++;
+                late=0;
+                if(absent>1){
+                   return false;
+                }
+            }else if(chars[i]=='L'){
+                late++;
+                if(late>2){
+                    return false;
+                }
+            }else {
+                late=0;
+            }
+        }
+        return true;
+    }
+}
