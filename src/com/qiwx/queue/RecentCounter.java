6394fbaa8238603e7f845f26a320daaca26cd9b6
qiwx <qiwx@jingzhengu.com>
2019-02-11 15:11:50 +0800

    基本计算器 II

---
+++ src/com/qiwx/queue/RecentCounter.java
@@ -0,0 +1,19 @@
+package com.qiwx.queue;
+
+import java.util.LinkedList;
+
+//最近请求次数
+public class RecentCounter {
+    LinkedList<Integer> queue;
+    public RecentCounter(){
+        queue=new LinkedList<>();
+    }
+    public int ping(int t){
+      queue.addLast(t);
+      t=t-3000;
+      while (queue.getFirst()<t){//如果是3000毫秒区间外的去掉
+          queue.removeFirst();
+      }
+      return queue.size();
+    }
+}
