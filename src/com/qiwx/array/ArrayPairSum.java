aa4d48e13f420aefc6807ac93eefca8941998e1c
qiwx <476364523@qq.com>
2019-02-24 21:54:19 +0800

    数组拆分1

---
+++ src/com/qiwx/array/ArrayPairSum.java
@@ -0,0 +1,21 @@
+package com.qiwx.array;
+
+import java.util.Arrays;
+
+//数组拆分 I
+public class ArrayPairSum {
+    public static void main(String[] args) {
+
+    }
+    public static int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int count=0;
+        int len=nums.length;
+        for(int i=0;i<len;i+=2){
+            count+=nums[i];
+        }
+        return count;
+
+    }
+
+}
