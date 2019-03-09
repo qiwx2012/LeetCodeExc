30f51baa74af577dd2cce18c6caad5f33f3d7951
qiwx <476364523@qq.com>
2019-03-09 16:40:24 +0800

    二叉树最大深度

---
+++ src/com/qiwx/tree/MaxDepth.java
@@ -0,0 +1,20 @@
+package com.qiwx.tree;
+
+import com.qiwx.model.TreeNode;
+
+import java.util.Stack;
+
+//二叉树最大深度
+public class MaxDepth {
+    public static void main(String[] args) {
+
+    }
+    int result = 0;
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        result = Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+        return result;
+    }
+}
