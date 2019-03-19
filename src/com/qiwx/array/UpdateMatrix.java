8ad3e17995a3b820f0b58370c16ebabce91afd0b
qiwx <qiwx@jingzhengu.com>
2019-03-19 11:36:26 +0800

    01矩阵

---
+++ src/com/qiwx/array/UpdateMatrix.java
@@ -0,0 +1,40 @@
+package com.qiwx.array;
+//01 矩阵
+public class UpdateMatrix {
+    public static void main(String[] args) {
+        updateMatrix(new int[][]{{0,0,0},{0,1,0},{1,1,1}});
+    }
+    public static int[][] updateMatrix(int[][] matrix) {
+        int row;
+        int col;
+        row = matrix.length;
+        col = matrix[0].length;
+        // 第一次遍历，正向遍历，根据相邻左元素和上元素得出当前元素的对应结果
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (matrix[i][j] == 1) {
+                    matrix[i][j] = row + col;
+                }
+                if (i > 0) {
+                    matrix[i][j] = Math.min(matrix[i][j], matrix[i - 1][j] + 1);
+                }
+                if (j > 0) {
+                    matrix[i][j] = Math.min(matrix[i][j], matrix[i][j - 1] + 1);
+                }
+            }
+        }
+        // 第二次遍历，反向遍历，根据相邻右元素和下元素及当前元素的结果得出最终结果
+        for (int i = row - 1; i >= 0; i--) {
+            for (int j = col - 1; j >= 0; j--) {
+                if (i < row - 1) {
+                    matrix[i][j] = Math.min(matrix[i][j], matrix[i + 1][j] + 1);
+                }
+                if (j < col - 1) {
+                    matrix[i][j] = Math.min(matrix[i][j], matrix[i][j + 1] + 1);
+                }
+            }
+        }
+        return matrix;
+
+    }
+}
