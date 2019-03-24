470ad5e72df7d6f0dcd468051d93165def0a7cea
qiwx <476364523@qq.com>
2019-03-24 21:37:44 +0800

    Excel表列序号

---
+++ src/com/qiwx/string/TitleToNumber.java
@@ -0,0 +1,23 @@
+package com.qiwx.string;
+//Excel表列序号
+public class TitleToNumber {
+    public static void main(String[] args) {
+        System.out.println(titleToNumber("A"));
+        System.out.println(titleToNumber("B"));
+        System.out.println(titleToNumber("AB"));
+        System.out.println(titleToNumber("ZY"));
+    }
+    public static int titleToNumber(String s) {
+        char[] chars=s.toCharArray();
+        int count=0;
+        int ratio=0;
+        for(int i=chars.length-1;i>=0;i--){
+            count+=(chars[i]-'A'+1)*((int)Math.pow(26,ratio));
+            ratio++;
+
+        }
+        return count;
+
+
+    }
+}
