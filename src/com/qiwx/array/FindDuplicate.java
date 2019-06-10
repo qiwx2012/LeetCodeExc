76522d5644fba34d7caaa24de3dc97cda64c40f0
qiwx <qiwx@jingzhengu.com>
2019-06-10 16:03:11 +0800

    ss

---
+++ src/com/qiwx/array/FindDuplicate.java
@@ -0,0 +1,21 @@
+package com.qiwx.array;
+//287. 寻找重复数
+public class FindDuplicate {
+    public static void main(String[] args) {
+      System.out.println(findDuplicate(new int[]{1,3,4,2,2}));
+    }
+    public static int findDuplicate(int[] nums) {
+        int len=nums.length;
+        int[] temp=new int[len];
+        for(int i=0;i<len;i++){
+            if(temp[nums[i]-1]==0){
+                temp[nums[i]-1]=nums[i];
+            }else {
+                return nums[i];
+            }
+        }
+        return 0;
+
+
+    }
+}
---
+++ src/com/qiwx/greedy/CanCompleteCircurit.java
@@ -0,0 +1,20 @@
+package com.qiwx.greedy;
+
+public class CanCompleteCircurit {
+    public static void main(String[] args) {
+
+    }
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int total = 0, sum = 0, start = 0;
+        for (int i = 0; i < gas.length; ++i) {
+            total += gas[i] - cost[i];
+            sum += gas[i] - cost[i];
+            if (sum < 0) {
+                start = i + 1;
+                sum = 0;
+            }
+        }
+        return (total < 0) ? -1 : start;
+
+    }
+}
