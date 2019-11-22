c52bb1e182899cdd78a6e44ad57d9264af32c988
qiwx <qiwx@jingzhengu.com>
2019-11-22 14:26:36 +0800

    最接近三数之和

---
+++ src/com/qiwx/array/ThreeSumClosest.java
@@ -0,0 +1,37 @@
+package com.qiwx.array;
+
+import java.util.Arrays;
+
+//最接近三数字之和
+public class ThreeSumClosest {
+    public static void main(String[] args) {
+       //System.out.println(threeSumClosest(new int[]{-1,2,1,-4},1));
+       System.out.println(threeSumClosest(new int[]{-3,-2,-5,-4,3},-1));
+    }
+    public static int threeSumClosest(int[] nums, int target) {
+        int mostclosest=nums[0]+nums[1]+nums[2];
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+            int num=nums[i];
+            int start=i+1;
+            int end=nums.length-1;
+            while (start<end){
+              int  temp=num+nums[start]+nums[end];
+              if(temp==target){
+                  return temp;
+              }else if(temp<target){
+                  start++;
+              }else {
+                  end--;
+              }
+              if(Math.abs(temp-target)<Math.abs(mostclosest-target)){
+                  mostclosest=temp;
+              }
+            }
+        }
+
+        return  mostclosest;
+
+    }
+
+}
